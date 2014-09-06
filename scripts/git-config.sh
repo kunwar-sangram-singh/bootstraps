9e9f1949aef4716e9a534ebc79957a768db2145a
Kunwar Sangram Singh <kunwar.sangram@gmail.com>
2014-09-06 14:49:25 +0530

    git config

---
+++ scripts/git-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global user.name "Kunwar Sangram Singh"
+git config --global user.email kunwar.sangram@gmail.com
